80059288918a4b517273f114e418c8217de45d6f
v1per <14767021+ubreckner@users.noreply.github.com>
2024-12-04 01:31:23 +0100

    Day 3/2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -2,15 +2,15 @@ fun main() {
     val dayToday = 3
 
     val input = loadInput(dayToday)
-    val testInput = loadTestInput(dayToday, 1)
 
-    val dayTest = Day03(testInput)
+    val dayTest1 = Day03(loadTestInput(dayToday, 1))
+    val dayTest2 = Day03(loadTestInput(dayToday, 2))
     val day = Day03(input)
     // test if implementation meets criteria from the description, like:
-    check(dayTest.part1() == 161)
+    check(dayTest1.part1() == 161)
     day.part1().println()
 
-    check(dayTest.part2() == 0)
+    check(dayTest2.part2() == 48)
     day.part2().println()
 }
 
@@ -33,6 +33,20 @@ class Day03(input: List<String>) {
 
     fun part2(): Int {
         var sum = 0
+        val regex = Regex("""mul\((\d+),(\d+)\)|(do\(\))|don't\(\)""")
+        var active = true
+        for (line in data) {
+            val res = regex.findAll(line)
+            for (match in res) {
+                if (match.value == "do()") {
+                    active = true
+                } else if (match.value == "don't()") {
+                    active = false
+                } else if (active) {
+                    sum += match.groupValues[1].toInt().times(match.groupValues[2].toInt())
+                }
+            }
+        }
         return sum
     }
 }
